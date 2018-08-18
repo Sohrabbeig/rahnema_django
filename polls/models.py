0b3511c9c1fc527ec4468ad348c5363a1a7908d9
amirhossein <Ahsohrabbeig@gmail.com>
2018-08-18 11:25:25 +0430

    Initial commit

---
+++ polls/models.py
@@ -0,0 +1,32 @@
+import datetime
+from django.contrib.auth.models import User
+from django.db import models
+from django.utils import timezone
+
+
+class Question(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return "Question: %s - authored by %s" %(self.question_text, self.author.username)
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+    # def delete(self, using=None, keep_parents=False):
+    #     self.is_active = False
+    #     self.save()
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(
+        Question, on_delete=models.CASCADE
+    )
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
